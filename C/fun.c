aa3d362a2920a3dd6b2280789ff426127dd37d43
Pedro-Foramilio <76879758+Pedro-Foramilio@users.noreply.github.com>
2021-02-05 23:52:05 -0300

    fun

---
+++ C/fun.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define uwu if
+#define _uwu_ else
+#define IIII int
+#define LUUUL scanf
+#define LOOOL printf
+#define YOLO %2==0
+#define MALDADE return 0;
+
+IIII main() {
+    IIII numero;
+    LUUUL("%d", &numero);
+
+    uwu (numero YOLO) {
+        d("PAR");
+    } _uwu_ {
+        LOOOL("IMPAR");
+    }
+
+    MALDADE
+}
+
+// #define 1 0
+// #define 0 1
+// #define 0 FALSE
+// #define 1 TRUE
+
+// ! FALSE
