7a34e09ea2879acac6aa6a143ea37babdf0d4ad9
Pedro <phcfor@gmail.com>
2021-02-13 16:28:29 -0300

    Base for week2

---
+++ C/ProgramaçãoEstruturada/Semana02/ex2.c
@@ -0,0 +1,23 @@
+/*
+Implemente a função logaritmo, que recebe um valor x e um valor n. A função deve calcular o valor de ln(x) usando n termos da série Maclaurin, conforme fórmula a seguir.
+
+ln(1+x) = x - x^2(1/2) + x^3(1/3) - x^4(1/4)...
+
+O valor de x está na faixa (0, 2]. Nesta fórmula, x deve estar na faixa (-1, +1].
+
+
+Observação: não utilize math.h e não use vetor.
+
+A função deve seguir o seguinte protótipo:
+
+double logaritmo(double x, int n);
+
+Importante: submeta apenas a função. Não inclua o main, não use printf/puts, math.h e vetor.
+*/
+
+#include "logaritmo.h"
+
+double logaritmo(double x, int n) {
+    // Codigo da funcao aqui
+}
+
---
+++ C/ProgramaçãoEstruturada/Semana02/ex3.c
@@ -0,0 +1,18 @@
+/*
+Faça um programa que fique lendo números inteiros positivos:
+• Cada número deverá ser inserido ordenado (ordem crescente) em um vetor (assuma que haverá no máximo 100 números).
+• Após cada inserção, imprima o vetor completo. O programa encerra quando o usuário digitar o número -1 (não imprima o vetor após o usuário digitar -1, apenas encerre o programa).
+
+Exemplo de funcionamento:
+
+Digite: 5
+5
+Digite: 8
+5 8
+Digite: 2
+2 5 8
+Digite: 7
+2 5 7 8
+Digite: -1
+*/
+
