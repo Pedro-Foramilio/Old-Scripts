81e312b788e324917b81a1541488a7120bcd2930
Pedro-Foramilio <76879758+Pedro-Foramilio@users.noreply.github.com>
2021-02-26 22:29:39 -0300

    ex5.c

--- C/ProgramaçãoEstruturada/Semana03/ex5.c
+++ C/ProgramaçãoEstruturada/Semana03/ex5.c
@@ -8,7 +8,48 @@ Importante: submeta apenas a função. Não use variáveis globais. Não inclua 
 */
 
 //#include "remover_palavra.h"
+#include <stdio.h>
 
 void remover_palavra(int i, char frase[]) {
+    int tamanho = 0;
+    int inicio = 0;
+    int final = 0;
+    int qtdEpacos = 0;
+    int j = 0;
+    
+    while (frase[tamanho] != '\0'){
+        tamanho++;
+    }
 
+    while (j < tamanho) {
+        if (frase[j] == ' ') {
+            qtdEpacos++;
+            if (qtdEpacos == i - 1) {
+                inicio = j;
+                break;
+            }
+        }
+        j++;
+    }
+
+    final = inicio + 1;
+    while (frase[final] != ' ' && frase[final] != '\0' && frase[final] != '.' 
+            && frase[final] != ',' && frase[final] != ';' && frase[final] != '\n') {
+        final++;
+    }
+
+    if (i == 1) {
+        final++;
+    }
+
+    for (int k = inicio; k <= tamanho; k++) {
+        frase[k] = frase[final];
+        final++;
+    }
+}
+
+int main() {
+    char frase[100] = "Neste exercicio, a funcao envolve uso de string.\0";
+    remover_palavra(2, frase);
+    puts(frase);
 }
