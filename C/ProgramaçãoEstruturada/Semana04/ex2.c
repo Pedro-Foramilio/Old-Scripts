d5b899d683b6e985c7e817a1f65687df2db713af
= <phcfor@gmail.com>
2021-03-05 17:41:53 -0800

    ex2.c OK

--- C/ProgramaçãoEstruturada/Semana04/ex2.c
+++ C/ProgramaçãoEstruturada/Semana04/ex2.c
@@ -16,5 +16,8 @@ Teste 2
 //#include "dec2bin.h"
 
 int dec2bin(int n_decimal) {
-
+    if (n_decimal == 0)
+        return 0;
+    else
+        return (n_decimal % 2) + dec2bin(n_decimal/2) * 10; 
 }
