3b61036018eae90c72ce8e6dff7451bb2c77d2d3
Pedro-Foramilio <76879758+Pedro-Foramilio@users.noreply.github.com>
2021-03-17 11:55:41 -0300

    ex1

--- C/ProgramaçãoEstruturada/testes.c
+++ C/ProgramaçãoEstruturada/testes.c
@@ -1,7 +1,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+/*
 void conta_linhas(char texto[], int largura_linha, int* qtd_linhas) {
     *qtd_linhas = 0;
 
@@ -48,4 +48,20 @@ int main() {
     free(qtd_linha);
 
     return 0;    
+}
+*/
+
+int main() {
+    int *qtd = malloc(sizeof(int));
+    *qtd = 0;
+
+    printf("qtd = %p, *qtd = %d\n", qtd, *qtd); // qtd = 007318C0, *qtd = 0
+    (*qtd)++;
+    printf("qtd = %p, *qtd = %d\n", qtd, *qtd); // qtd = 007318C0, *qtd = 1
+    *qtd++;
+    printf("qtd = %p, *qtd = %d\n", qtd, *qtd); // qtd = 007318C4, *qtd = 7536832
+    *++qtd;
+    printf("qtd = %p, *qtd = %d\n", qtd, *qtd); // qtd = 007318C8, *qtd = -1797823864
+    ++*qtd;
+    printf("qtd = %p, *qtd = %d\n", qtd, *qtd); // qtd = 007318C8, *qtd = -1797823863
 }
