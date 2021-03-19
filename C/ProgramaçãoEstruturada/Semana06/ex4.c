0486b5023a9c34fc34bf38dcc23b8fe0b2256645
= <phcfor@gmail.com>
2021-03-18 17:18:37 -0700

    ex4 OK

--- C/ProgramaçãoEstruturada/Semana06/ex4.c
+++ C/ProgramaçãoEstruturada/Semana06/ex4.c
@@ -21,4 +21,77 @@ Importante: o programa não pode usar colchetes, portanto, será necessário uti
 ponteiros para acessar os elementos dos vetores. Para alocar os vetores, use malloc.
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubbleSort(int *v, int tamanho) {
+    int aux = 0;
+    for (int i = tamanho; i > 0; i--) {
+        for (int j = 0; j < i -1; j++) {
+            if (*(v+j) > *(v+j+1)) {
+                aux = *(v+j);
+                *(v+j) = *(v+j+1);
+                *(v+j+1) = aux;
+                aux = 0;
+            }
+        }
+    }  
+}
+
+int main() {
+
+    int n1;
+    scanf("%d", &n1);
+    int *vetor1 = calloc(n1, sizeof(int));
+    for (int i = 0; i < n1; i++) {
+        scanf("%d", (vetor1 + i));
+    }
+
+    int n2;
+    scanf("%d", &n2);
+    int *vetor2 = calloc(n2, sizeof(int));
+    for (int i = 0; i < n2; i++) {
+        scanf("%d", (vetor2 + i));
+    }
+
+    int *vetorFinal = calloc((n1+n2), sizeof(int));
+    int k = 0;
+    for (int i = 0; i < n1; i++) {
+        *(vetorFinal+i) = *(vetor1 + i);
+        k++;
+    }
+    for (int i=0; i < n2; i++) {
+        *(vetorFinal+k) = *(vetor2 + i);
+        k++;
+    }
+
+    bubbleSort(vetorFinal, (n1+n2));
+
+    for (int i=0; i < n1+n2; i++) {
+        printf("%d ", *(vetorFinal+i));
+    }
+
+
+    /*
+    while ( (k < n1) || (k < n2)) {
+
+        if (k < n1){
+            printf("%d ", *(vetor1 + k));
+        }
+
+        if (k < n2){
+            printf("%d ", *(vetor2 + k));
+        }
+        k++;
+    }
+    */
+    printf("\n");
+
+    free(vetor1);
+    free(vetor2);
+    free(vetorFinal);
+    return 0;
+}
+
+
 
