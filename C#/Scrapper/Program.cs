9b4bd681eb354bf9992e14a076dba59d35e68d67
= <phcfor@gmail.com>
2021-04-27 19:50:39 -0700

    c# scrapping template

---
+++ C#/Scrapper/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Scrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller controller = new Controller();
+            String body = Controller.getUrlBody();
+            Console.WriteLine(body);
+        }
+    }
+}
